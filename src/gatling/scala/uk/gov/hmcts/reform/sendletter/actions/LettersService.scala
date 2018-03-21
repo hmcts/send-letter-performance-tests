f5005cf3d7482aa397f7493ec9d0c2443aca0de8
Rafal Kondratowicz <rkondr@gmail.com>
2018-03-21 10:41:41 +0000

    Update template

--- src/gatling/scala/uk/gov/hmcts/reform/sendletter/actions/LettersService.scala
+++ src/gatling/scala/uk/gov/hmcts/reform/sendletter/actions/LettersService.scala
@@ -1,47 +1,52 @@
 package uk.gov.hmcts.reform.sendletter.actions
 
+import java.util.UUID
+
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ChainBuilder
-import io.gatling.http.Predef._
-import io.gatling.http.HeaderValues._
 import io.gatling.http.HeaderNames._
+import io.gatling.http.HeaderValues._
+import io.gatling.http.Predef._
 
 object LettersService {
 
+  private val uuidFeeder = Iterator.continually(Map("uuid" -> UUID.randomUUID.toString))
+
   val create: ChainBuilder =
-    exec(
-      http("Create letter")
-        .post("/letters")
-        .headers(Map(
-          "ServiceAuthorization" -> "Bearer ${service_token}",
-          ContentType -> ApplicationJson
-        ))
-        .body(StringBody(
-          """
-            |{
-            |  "documents": [
-            |    {
-            |      "template": "abc",
-            |      "values": {
-            |        "a": "b"
-            |      }
-            |    },
-            |    {
-            |      "template": "xyz",
-            |      "values": {
-            |        "x": "y"
-            |      }
-            |    }
-            |  ],
-            |  "type": "someType"
-            |}
-          """.stripMargin
-        ))
-        .check(
-          status.is(200),
-          jsonPath("$.letter_id").saveAs("id")
-        )
-    )
+    feed(uuidFeeder)
+      .exec(
+        http("Create letter")
+          .post("/letters")
+          .headers(Map(
+            "ServiceAuthorization" -> "Bearer ${service_token}",
+            ContentType -> ApplicationJson
+          ))
+          .body(StringBody(
+            """
+              |{
+              |  "documents": [
+              |    {
+              |      "template": "<html>hello {{foo}}</html>",
+              |      "values": {
+              |        "foo": "${uuid}"
+              |      }
+              |    },
+              |    {
+              |      "template": "<html>hello again {{foo}}</html>",
+              |      "values": {
+              |        "foo": "bar"
+              |      }
+              |    }
+              |  ],
+              |  "type": "someType"
+              |}
+            """.stripMargin
+          ))
+          .check(
+            status.is(200),
+            jsonPath("$.letter_id").saveAs("id")
+          )
+      )
 
   val checkStatus: ChainBuilder =
     exec(

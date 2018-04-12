98744717043c65c17c350fb67701397c651ade7a
Rafal Kondratowicz <rkondr@gmail.com>
2018-04-12 23:35:18 +0200

    Move otp feeder to where it's used

--- src/gatling/scala/uk/gov/hmcts/reform/sendletter/actions/S2s.scala
+++ src/gatling/scala/uk/gov/hmcts/reform/sendletter/actions/S2s.scala
@@ -1,21 +1,28 @@
 package uk.gov.hmcts.reform.sendletter.actions
 
+import com.typesafe.config.ConfigFactory
+import com.warrenstrange.googleauth.GoogleAuthenticator
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ChainBuilder
 import io.gatling.http.Predef._
-import com.typesafe.config.ConfigFactory
 import uk.gov.hmcts.reform.sendletter
 
 object S2s {
   private val config = ConfigFactory.load()
 
-  val leaseServiceToken: ChainBuilder = exec(
-    sendletter.applyOptionalProxy(
-      http ("Lease service token")
-      .post(config.getString("s2sUrl") + "/lease")
-      .body(StringBody("""{"microservice":"""" + config.getString("service.name") + """","oneTimePassword":"${otp}"}"""))
-      .asJSON
-      .check(bodyString.saveAs("service_token"))
-    )
-  )
+  private val authenticator: GoogleAuthenticator = new GoogleAuthenticator()
+
+  private val otpFeeder = Iterator.continually(Map("otp" -> authenticator.getTotpPassword(config.getString("service.pass"))))
+
+  val leaseServiceToken: ChainBuilder =
+    feed(otpFeeder)
+      .exec(
+        sendletter.applyOptionalProxy(
+          http("Lease service token")
+            .post(config.getString("s2sUrl") + "/lease")
+            .body(StringBody("""{"microservice":"""" + config.getString("service.name") + """","oneTimePassword":"${otp}"}"""))
+            .asJSON
+            .check(bodyString.saveAs("service_token"))
+        )
+      )
 }
